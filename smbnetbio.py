96a9ba618da8c9cafc764f4a0408ce4e328d8465
PawelKozy <46841983+PawelKozy@users.noreply.github.com>
2024-05-16 15:57:18 +0200

    Update smbnetbio.py

--- smbnetbio.py
+++ smbnetbio.py
@@ -1,41 +1,41 @@
 from impacket.smbconnection import SMBConnection
-from impacket.dcerpc.v5 import srvs, scmr
+from impacket.dcerpc.v5 import transport, scmr
 
-def create_service_and_run(conn, service_name, executable_path):
-    rpc_transport = scmr.DCERPCTransportFactory(r'ncacn_np:%s[\pipe\svcctl]' % conn.getRemoteHost())
-    rpc_transport.set_dport(139)
-    rpc_transport.setRemoteName(conn.getRemoteName())
-    rpc_transport.set_credentials(conn._username, conn._password, conn._domain, conn._lmhash, conn._nthash)
-    
+def create_service_and_run(remote_host, username, password, domain, service_name, executable_path):
+    # Establish SMB Connection using the same remote_host for simplicity
+    conn = SMBConnection(remote_host, remote_host, sess_port=139)
+    conn.login(username, password, domain=domain)
+
+    # Set up the transport for the DCERPC session over SMB
+    string_binding = r'ncacn_np:{}[\pipe\svcctl]'.format(remote_host)
+    rpc_transport = transport.DCERPCTransportFactory(string_binding)
+    rpc_transport.set_credentials(username, password, domain)
+
     dce = rpc_transport.get_dce_rpc()
     dce.connect()
     dce.bind(scmr.MSRPC_UUID_SCMR)
-    
+
     # Open SCManager
-    resp = scmr.hROpenSCManagerW(dce)
-    sc_manager_handle = resp['lpScHandle']
+    sc_manager_handle = scmr.hROpenSCManagerW(dce)['lpScHandle']
     
     # Create the service
-    resp = scmr.hRCreateServiceW(dce, sc_manager_handle, service_name+'\x00', service_name+'\x00', lpBinaryPathName=executable_path+'\x00')
-    service_handle = resp['lpServiceHandle']
+    create_resp = scmr.hRCreateServiceW(dce, sc_manager_handle, service_name, service_name, lpBinaryPathName=executable_path)
+    service_handle = create_resp['lpServiceHandle']
     
     # Start the service
     scmr.hRStartServiceW(dce, service_handle)
-    
+
     # Properly close handles and disconnect
     scmr.hRCloseServiceHandle(dce, service_handle)
     scmr.hRCloseServiceHandle(dce, sc_manager_handle)
     dce.disconnect()
 
-# Connection info
+# Example usage
 target_ip = '192.168.1.100'
 username = 'admin'
 password = 'password'
 domain = 'DOMAIN'
+service_name = 'MyRemoteService'
+executable_path = r'C:\Windows\System32\notepad.exe'
 
-# Connection setup
-conn = SMBConnection(target_ip, target_ip, sess_port=139)
-conn.login(username, password, domain=domain)
-
-# Create and start service
-create_service_and_run(conn, 'MyRemoteService', r'C:\Windows\System32\notepad.exe')
+create_service_and_run(target_ip, username, password, domain, service_name, executable_path)
